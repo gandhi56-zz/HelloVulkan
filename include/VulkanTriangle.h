79e993816ef3bf97671f3d796fff24a7124d03e6
gandhi56 <gandhi21299@gmail.com>
2021-02-01 18:22:06 -0700

    adding validation layers

--- include/VulkanTriangle.h
+++ include/VulkanTriangle.h
@@ -12,15 +12,30 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <memory>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation"};
 
-struct DestroyGLFWwindow{
-    void operator()(GLFWwindow* ptr){
-        glfwDestroyWindow(ptr);
-    }
-};
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance,
+                                      const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+                                      const VkAllocationCallbacks* pAllocator,
+                                      VkDebugUtilsMessengerEXT* pDebugMessenger);
+void DestroyDebugUtilsMessengerEXT(VkInstance instance,
+                                   VkDebugUtilsMessengerEXT debugMessenger,
+                                   const VkAllocationCallbacks* pAllocator);
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                                                    VkDebugUtilsMessageTypeFlagsEXT messageType,
+                                                    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+                                                    void* pUserData);
 
 class VulkanTriangle {
 public:
@@ -34,12 +49,17 @@ public:
 private:
     GLFWwindow* window;
     VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
 
     void initWindow();
     void initVulkan();
     void mainLoop();
     void cleanup();
     void createInstance();
+    void setupDebugMessenger();
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+    bool checkValidationLayerSupport();
+    std::vector<const char*> getRequiredExtensions();
 };
 
 
