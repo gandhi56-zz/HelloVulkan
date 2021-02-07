aee73263e5463a784a678d80f2704d1ad9f05620
gandhi56 <gandhi21299@gmail.com>
2021-02-07 01:28:17 -0700

    created graphics pipeline

--- include/VulkanTriangle.h
+++ include/VulkanTriangle.h
@@ -81,6 +81,10 @@ private:
 
   std::vector<VkImageView> swapChainImageViews;
 
+  VkRenderPass renderPass;
+  VkPipelineLayout pipelineLayout;
+  VkPipeline graphicsPipeline;
+
   void initWindow();
   void initVulkan();
   void mainLoop();
@@ -104,6 +108,7 @@ private:
   void createImageViews();
   void createGraphicsPipeline();
   VkShaderModule createShaderModule(const std::vector<char>& code);
+  void createRenderPass();
 };
 
 
